b60b8e5b586e453517c27563fd20d0b2a3572f07
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-11-20 19:10:32 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,60 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useState } from 'react';
+import Layout from '../components/Layout';
+import { Pokemon } from '../interfaces/pokemon';
+import style from '../components/Pokemon.module.css';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([
+    {
+      name: 'ピカチュウ',
+      life: 100,
+    },
+    {
+      name: 'フシギバナ',
+      life: 100,
+    },
+    {
+      name: 'ヒトカゲ',
+      life: 100,
+    },
+  ]);
 
-export default IndexPage
+  const atack = (index: number) => {
+    pokemons[index].life -= 10;
+    setPokemons([...pokemons]);
+  };
+
+  const heal = (index: number) => {
+    pokemons[index].life += 10;
+    setPokemons([...pokemons]);
+  };
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>
+            <p>{pokemon.name}</p>
+            <div className={style.life}>
+              <div
+                className={`${style.life__body} ${
+                  pokemon.life < 30 && style.denger
+                }`}
+                style={{ transform: 'scaleX(' + pokemon.life / 100 + ')' }}
+              ></div>
+            </div>
+            <button disabled={pokemon.life < 10} onClick={() => atack(index)}>
+              攻撃
+            </button>
+            <button disabled={pokemon.life > 90} onClick={() => heal(index)}>
+              回復
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export default IndexPage;
